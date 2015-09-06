43703d947b1bca3b6150ba22575584c3afcef2fb
albizures <albizures3601@gmail.com>
2015-09-05 21:19:28 -0600

    cambios en estructura y explicacion en el readme

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # umg-gauss
+
+En el archivo index se llaman a los archivos
+
+* gauss,js
+* resta.js
+* suma.js
+* multiplicacion.js
+* init.js
+
+En init se comienza la app y se llama a cada function de las operaciones.
+
+Estan en archivos diferentes para faciliar los cambios con git asi trabajar cada quien en su archivo.
+
+
+Para el html hay un div que hace el de menu y otros abajo que es el area donde se tienen que poner los inputs y demas cosas para cada operacion, todo el contenido tiene que estar dentro de un  `<div class="card hoverable">aaa</div>` en cada seccion respectiva
---
+++ js/init.js
@@ -0,0 +1,84 @@
+(function($,_){
+	$.estado = {
+		animation : false
+	}
+	$.ecuaciones = [];
+	$.colors = {
+		'resta' : 'orange darken-2',
+		'suma' : 'green darken-2',
+		'gauss' : 'blue-grey',
+		'multiplicacion' : 'deep-orange darken-2'
+	}
+	/*window.onload = function(){
+		var ecuacion = _.getElementById('ecuacion'),
+			agregar = _.getElementById('agregar');
+		agregar.addEventListener('click',function(){
+			if(ecuaciones.length ){
+				console.log('se agrego una nueva');
+			}
+			ecuaciones.push(ecuacion.value);
+
+			console.log(ecuaciones);
+		});
+	};*/
+})(window,document);
+
+
+
+$(onLoad);
+function onLoad () {
+	$('.collection-item').click(onClickCollectionItem);
+	$(window).click(onClickWindow);
+	gauss();
+	suma();
+	resta();
+	multiplicacion();
+}
+function onClickCollectionItem (event) {
+	if(estado.animation) return;
+	var seccion = $(this).attr('class').replace('collection-item','').trim();
+	var parent = $(this).parents('main');
+	var self = this;
+	if(!$(parent).hasClass('active')){
+		estado.animation = true;
+		$(parent).toggleClass('active');
+		setTimeout(function () {
+			$(parent).toggleClass('second');
+			$(parent).addClass(colors[seccion]);
+		},500);
+		setTimeout(function () {
+			$('.seccion.'+seccion).toggleClass('active');
+			$('.close').toggleClass('active');
+			setTimeout(function () {
+				estado.animation = false;
+			},500);
+		},900);
+	}
+}
+function onClickWindow (event) {
+	console.log('estado ',estado.animation);
+	if(estado.animation) return;
+	console.log('despues ',estado.animation);
+	if($(event.target).hasClass('seccion') || $(event.target).hasClass('close')){
+		console.log('cambio');
+		estado.animation = true;
+		$('.close').toggleClass('active');
+		$('.seccion.active').toggleClass('active');
+		setTimeout(function () {
+				$('main.item').toggleClass('second');
+				for(var index in colors){
+						$('main.item').removeClass(colors[index]);
+				}
+		},500)
+		setTimeout(function () {
+			$('main.item').toggleClass('active');
+			setTimeout(function () {
+				estado.animation = false;
+			},500);
+		},900);
+	}
+}
+
+/*
+"abc def+(1.1+{ghi})".match(/[^+/*()-]+/g).filter(
+    function(x) { return !/^{.+?}$/.test(x) })*/
